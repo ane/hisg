f6e5b9e780b6174b7e8458f09f288cd5afa649b6
anhekalm <ane@fucking-awesome.org>
2009-12-19 13:49:54 +0200

    Removed trailing whitespaces

--- Main.hs
+++ Main.hs
@@ -1,18 +1,22 @@
 module Main where
 
 import Data.List
+import Data.Maybe
 import Text.ParserCombinators.Parsec
 
 import Types
 import LogParser
+import Analyzer
 
 main = do
-          file <- readFile "tiea341.log"
-          mapM_ (printLine) (lines (file))
-       where
-          printLine str = case decode (str ++ "\n") of 
-                            Just s -> 
+            file <- readFile "tiea341.log"
+            let decoded = map (\s -> decode (s ++ "\n")) (lines file)
+            --mapM_ (printLine) (decoded)
+            putStrLn (show (countLines "ibid" (map (fromMaybe (Simple "")) (decoded))))
+        where
+            printLine s = case s of
+                            Just s ->
                                 putStrLn (show s)
-                            Nothing -> 
+                            Nothing ->
                                 putStrLn "Parse error lol"
 
