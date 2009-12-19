2326b0809ea43166145aa7b3858dcaa4af860280
anhekalm <ane@fucking-awesome.org>
2009-12-19 12:59:35 +0200

    Modified analyzer

--- Analyzer.hs
+++ Analyzer.hs
@@ -1,4 +1,11 @@
 module Analyzer where
 
+import Types
 
+countLines :: String -> [Logline] -> Int
+countLines nick log = length matched
+    where
+        matched = filter (matchNick nick) log
 
+matchNick nick (Message ts nick' cont) = nick == nick'
+matchNick _ _ = False
