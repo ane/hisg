c2261f127197584940234bfa06b76df9fc3f725b
anhekalm <ane@fucking-awesome.org>
2009-12-19 20:00:23 +0200

    Improved the analyzer

--- Main.hs
+++ Main.hs
@@ -9,14 +9,16 @@ import LogParser
 import Analyzer
 
 main = do
-            file <- readFile "tiea341.log"
-            let decoded = map (\s -> decode (s ++ "\n")) (lines file)
-            --mapM_ (printLine) (decoded)
-            putStrLn (show (countLines "ibid" (map (fromMaybe (Simple "")) (decoded))))
-        where
-            printLine s = case s of
-                            Just s ->
-                                putStrLn (show s)
-                            Nothing ->
-                                putStrLn "Parse error lol"
+    file <- readFile "tiea341.log"
+    let decoded = map (\s -> decode (s ++ "\n")) (lines file)
+    putStr "Enter nickname: "
+    nick <- getLine
+    putStrLn (show (runAnalyzer (countLines nick) (map (fromMaybe (Simple "")) decoded)))
+    putStrLn (show (runAnalyzer (countWords nick) (map (fromMaybe (Simple "")) decoded)))
+    putStrLn (show (runAnalyzer (countEvents nick "JOIN") (map (fromMaybe (Simple "")) decoded)))
+    putStrLn (show (runAnalyzer (countEvents nick "") (map (fromMaybe (Simple "")) decoded)))
+  where
+    printLine s = case s of
+      Just s  -> putStrLn (show s)
+      Nothing -> putStrLn "Parse error lol"
 
--- Types.hs
+++ Types.hs
@@ -4,59 +4,33 @@ import Data.List
 import Data.Char
 import Text.Printf
 
-data Timestamp =
-	Timestamp
-		{
-          ts_hour :: Int,
-		  ts_minute :: Int
-		}
-
-data Date =
-    Date
-        {
-            date_day    :: String,
-            date_month  :: String,
-            date_year   :: String
-        }
-
-data User =
-	User {
-			user_nickname 	:: String,
-			user_words	 	:: Int,
-			user_lines		:: Int
-		 }
-
-data Event = Event {
-            event_ts        :: Timestamp,
-            event_type      :: EventType,
-            event_user      :: String,
-            event_host      :: String,
-            event_param     :: String
-         }
-
-data EventType = Join | Part | Kick | Ban | Quit | Topic | Nick | Misc deriving(Show)
+-- Basic types
+data Timestamp = Timestamp { ts_hour :: Int, ts_minute :: Int }
+data Date = Date { date_day  :: String, date_month :: String, date_year :: String }
+data User = User { user_nickname :: String, user_words :: Int, user_line :: Int }
+data Event = Event { event_ts :: Timestamp, event_type :: EventType, event_user :: String, event_host :: String, event_param :: String }
 
 data Logline =
-	Message {
-			timestamp 	:: Timestamp,
-            nickname    :: String,
-			content		:: String
-		 }
-    -- Messages such as "Log file opened."
-    | Notification String
-    | DateChange Date
-    | LogEvent Event
-    | Simple String
+  Message { timestamp :: Timestamp, nickname  :: String, content :: String }
+  | Notification String
+  | DateChange Date
+  | LogEvent Event
+  | Simple String
+
+-- Type aliases
+
+type Logfile = [Logline]
+type EventType = String
 
 instance Show Timestamp where
 	show (Timestamp h m) = printf "%02d:%02d" h m
 
 instance Show Logline where
-    show (Message ts nick content) = show ts ++ " " ++ "<" ++ nick ++ ">" ++ " " ++ content
-    show (Notification cont) = cont
-    show (DateChange (Date d m y)) = intercalate " " [d, m, y]
-    show (LogEvent ev) = show ev
+  show (Message ts nick content) = show ts ++ " " ++ "<" ++ nick ++ ">" ++ " " ++ content
+  show (Notification cont) = cont
+  show (DateChange (Date d m y)) = intercalate " " [d, m, y]
+  show (LogEvent ev) = show ev
+  show (Simple str) = str
 
 instance Show Event where
-    show (Event ts evtype evuser host param) = map (toUpper) (show ts) ++ " " ++ show evtype ++ ": " ++ evuser ++ " (" ++ host ++ ")" ++ " -> " ++ param
-
+  show (Event ts evtype evuser host param) = map (toUpper) (show ts) ++ " " ++ show evtype ++ ": " ++ evuser ++ " (" ++ host ++ ")" ++ " -> " ++ param
