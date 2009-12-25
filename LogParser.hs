19b6bf6fb9ee8ef0f5a52253c3124c029fbd0792
anhekalm <ane@fucking-awesome.org>
2009-12-25 18:54:01 +0200

    Fixed typo in Main

--- LogParser.hs
+++ LogParser.hs
@@ -12,68 +12,68 @@ decode = (either (const Nothing) Just) . (parse line "")
 
 parseTimestamp :: CharParser st Timestamp
 parseTimestamp = do
-  h <- many1 digit
-  char ':'
-  m <- many1 digit
-  space
-  return (Timestamp (read h) (read m))
+    h <- many1 digit
+    char ':'
+    m <- many1 digit
+    space
+    return (Timestamp (read h) (read m))
 
 parseContent :: CharParser st String
 parseContent = anyChar `manyTill` (lookAhead (oneOf "\n"))
 
 line :: CharParser st Logline
 line = do
-  try parseEvent
-  <|> try parseUserMessage
-  <|> try parseNotification
-  <?> "Weird line"
+    try parseEvent
+    <|> try parseUserMessage
+    <|> try parseNotification
+    <?> "Weird line"
 
 parseUserMessage :: CharParser st Logline
 parseUserMessage = do
-  ts <- parseTimestamp
-  char '<'
-  status <- anyChar
-  nickName <- anyChar `manyTill` (lookAhead (oneOf ">"))
-  char '>'
-  space
-  cont <- parseContent
-  return (Message ts nickName cont)
+    ts <- parseTimestamp
+    char '<'
+    status <- anyChar
+    nickName <- anyChar `manyTill` (lookAhead (oneOf ">"))
+    char '>'
+    space
+    cont <- parseContent
+    return (Message ts nickName cont)
 
 parseNotification :: CharParser st Logline
 parseNotification = do
-  string "--- "
-  cont <- (try parseDayChange <|> parseContent)
-  return (Notification cont)
+    string "--- "
+    cont <- (try parseDayChange <|> parseContent)
+    return (Notification cont)
 
 parseDayChange :: CharParser st String
 parseDayChange = do
-  string "Day changed "
-  date <- parseContent
-  return ("DATE CHANGE: "++ date)
+    string "Day changed "
+    date <- parseContent
+    return ("DATE CHANGE: "++ date)
 
 parseEvent :: CharParser st Logline
 parseEvent = do
-  ts <- parseTimestamp
-  string "-!-"
-  space
-  user <- many1 letter
-  space
-  char '['
-  host <- anyChar `manyTill` (lookAhead (oneOf "]"))
-  char ']'
-  string " has "
-  evtype <- parseEventType
-  param <- parseContent
-  return (LogEvent (Event ts evtype user host param))
+    ts <- parseTimestamp
+    string "-!-"
+    space
+    user <- many1 letter
+    space
+    char '['
+    host <- anyChar `manyTill` (lookAhead (oneOf "]"))
+    char ']'
+    string " has "
+    evtype <- parseEventType
+    param <- parseContent
+    return (LogEvent (Event ts evtype user host param))
 
 parseEventType :: CharParser st EventType
 parseEventType = do
-  ev <- anyChar `manyTill` (lookAhead (oneOf " "))
-  space
-  return (case ev of
-    "joined"  -> "JOIN"
-    "left"    -> "PART"
-    "quit"    -> "QUIT"
-    _       ->   "")
+    ev <- anyChar `manyTill` (lookAhead (oneOf " "))
+    space
+    return (case ev of
+        "joined"    -> "JOIN"
+        "left"        -> "PART"
+        "quit"        -> "QUIT"
+        _             -> "")
 
 
