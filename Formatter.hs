4ce03e3924ba7cb5ff577b603a8e4c59f5ebb714
anhekalm <ane@fucking-awesome.org>
2009-12-25 23:43:11 +0200

    Refactored message stats and implemented a rudimentary HTML display Still writes to stdout, though.

--- Formatter.hs
+++ Formatter.hs
@@ -4,12 +4,19 @@ module Formatter (
 
 import Types
 
-usersToHTML :: [User] -> String -> IO ()
-usersToHTML users chan = do
+headers :: IO ()
+headers = do
     putStrLn "<html><head><style type=\"text/css\"> body { margin: 0px auto; text-align: center; font: 12px Arial, Helvetica, sans-serif; } table { margin: 0px auto; } td { padding: 5px; border: 1px solid #ccc; }</style></head><body>"
     putStrLn $ "<h1>Statistics for #" ++ chan ++ "</h1>"
+
+usersTable :: [User] -> String -> IO ()
+usersTable users chan = do
     putStrLn "<table>"
     putStrLn "<tr><th>Nick</th><th>Lines</th><th>Words</th></tr>"
     mapM_ (\u -> do putStrLn $ "<tr><td>" ++ user_nickname u ++ "</td><td>" ++ show (user_line u) ++ "</td><td>" ++ show (user_words u) ++ "</td></tr>"
         ) users
-    putStrLn "</table></body></html>"
+    putStrLn "</table>"
+
+footer :: IO ()
+footer = do
+    putStrLn "</body></html>"
--- Main.hs
+++ Main.hs
@@ -3,16 +3,17 @@ module Main where
 import Data.List
 import Data.Maybe
 import System (getArgs)
+import System.Console.GetOpt
 
 import Types
-import LogParser
+import Log
 import User
 import Misc
 import Formatter
 
 main = do
     args <- getArgs
-    file <- readFile (head args)
+    file <- readFile "battlegroup5.log"
     let decoded = map (fromMaybe (Simple "") . (decode . (++ "\n"))) (lines file)
-    --putStrLn (intercalate "\n" (map (show) (reverse $ qsort (buildUsers decoded))))
-    usersToHTML (reverse . qsort $ buildUsers decoded) "tiea341"
+    putStrLn (intercalate "\n" (map (show) (reverse $ qsort (getUserStats decoded))))
+    --usersToHTML (reverse . qsort $ buildUsers decoded) "tiea341"
