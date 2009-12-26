ad81cc25cb8e607651d65013607d2584746146b9
anhekalm <ane@fucking-awesome.org>
2009-12-26 21:42:54 +0200

    + Added command line arguments + Added a stylesheet + Generates kicks statistics @ Line/Word stats are now ranked. @ The Formatter module contains only IO operations now.

--- Misc.hs
+++ Misc.hs
@@ -1,4 +1,23 @@
-module Misc where
+module Misc (
+    qsort,
+    common,
+    mostCommon,
+    cmp
+    ) where
+
+import Data.List
 
 qsort [] = []
 qsort (x:xs) = qsort (filter (< x) xs) ++ [x] ++ qsort (filter (>= x) xs)
+
+cmp xs ys | length xs > length ys = LT
+          | otherwise = GT
+
+common :: (Ord a) => [a] -> [[a]]
+common [] = error "Empty list"
+common xs = let sorted = qsort xs in sortBy cmp $ group $ sorted
+
+mostCommon [] = error "Empty list!"
+mostCommon list@(x:xs) = head . common $ list
+
+
