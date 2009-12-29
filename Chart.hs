5825bc73d793f6d34e1210f06ee1c01bd3655a26
ane <ane@iki.fi>
2009-12-29 15:46:29 +0200

    Simplified the code a bit and made it faster. Still working on the major bottleneck which the user table. Perhaps making it all monadic might help...

--- Chart.hs
+++ Chart.hs
@@ -20,12 +20,19 @@ module Chart where
 
 import Data.List
 import Data.List.Split
-import Control.Arrow
+import Data.Maybe
+import Control.Monad
 import Text.Printf
 import System.IO
 
 import Types
 import Stats
+
+type Interval = String
+
+intervals = [("week", 7),
+             ("month", 30),
+             ("day", 1)]
 
 colors :: [String]
 colors = ["CCFFCC", "66CCFF", "0066FF", "0000CC"]
@@ -61,8 +68,8 @@ spliceDay log = [a, b, c, d]
         hours = getHours . getTimestamps $ log
 
 -- Proportions the number of lines spoken in a segment to the total number of lines spoken that day.
-proportionHours :: [[Int]] -> (Int, (Double, Double, Double, Double))
-proportionHours hours = (tot, h $ map prop hours)
+proportionPeriodHours :: [[Int]] -> (Double, Double, Double, Double)
+proportionPeriodHours hours = h $ map prop hours
     where
         h [a, b, c, d] = (a, b, c, d)
         tot = sum (map length hours)
@@ -71,21 +78,26 @@ proportionHours hours = (tot, h $ map prop hours)
             where
                 len = fromIntegral (length xs)
 
-analyzeHourly :: Int -> Log -> [[Int]]
-analyzeHourly interval log = calcAverages . catenateRows . chunk interval . toDailyHours $ (getDays log)
+-- Falls back to 1 if it doesn't find a good interval.
+-- Should use 7?
+getInterval :: Interval -> Int
+getInterval interval = fromMaybe 1 (lookup interval intervals)
+
+analyzeHourly :: Log -> Interval -> [[Int]]
+analyzeHourly log interval = calcAverages . catenateRows . chunk (getInterval interval) . toDailyHours $ getDays log
     where
         -- The moment I wrote this I knew my soul was damned forever.
         calcAverages = map (map (round . (*100) . avg))
-        catenateRows = map (conv4 . unzip4 . snd . unzip)
-        toDailyHours = map (proportionHours . spliceDay)
+        catenateRows = map (conv4 . unzip4)
+        toDailyHours = map (proportionPeriodHours . spliceDay)
 
 daysByWeeks xs interval = map length (map head . chunk interval $ xs)
 
-analyzeLineTrend :: [[LogEvent]] -> Int -> [Int]
+analyzeLineTrend :: [[LogEvent]] -> Interval -> [Int]
 analyzeLineTrend days interval = map propLen linesPerDay
     where
         -- Usually divided into 7 days.
-        linesPerDay = daysByWeeks days interval
+        linesPerDay = daysByWeeks days (getInterval interval)
         biggest = maximum linesPerDay
         propLen x = round (100 * (fromIntegral x / fromIntegral biggest))
 
@@ -99,30 +111,36 @@ genDataSet :: [Int] -> String
 genDataSet [] = ""
 genDataSet xs = intercalate "," (map show xs)
 
-genLineChartUrl out log = do
+genLineChartUrl :: Handle -> Log -> Interval -> IO ()
+genLineChartUrl out log interval = do
     let days = getDays log
-        crunched = analyzeLineTrend days 7
+        crunched = analyzeLineTrend days interval
         ylegend = intercalate "|" (map show [1 .. length crunched])
         dayLines = map length days
         biggest = maximum dayLines
-        average = (round $ fromIntegral (sum dayLines) / fromIntegral (length dayLines)) :: Int
-        prop = ((round (100 * (fromIntegral average / fromIntegral biggest))) :: Int)
+        average = round $ fromIntegral (sum dayLines) / fromIntegral (length dayLines) :: Int
+        prop = round (100 * (fromIntegral average / fromIntegral biggest)) :: Int
 
-    hPutStrLn out $ "<h2>Average lines per month</h2>" ++
+    hPutStrLn out $ "<h2>Average lines per "++interval++"</h2>" ++
                     "<img src=\"" ++
                     "http://chart.apis.google.com/chart?" ++
-                    "cht=lc&chs=500x250&chm=B,008B8B,0,0,0&chd=t:" ++
-                    (genDataSet crunched) ++
+                    "&chm=B,66CCFF,0,1,0" ++
+                    "&cht=lc&chs=500x250&chd=t:" ++
+                    genDataSet crunched ++
                     "&chco=445588&chxt=y,y,x,x,r&chxl=0:|lines|1:|" ++
-                    show biggest ++ "|2:|" ++ ylegend ++ "|3:|Month|4:|average " ++
+                    show biggest ++ "|2:|" ++ ylegend ++ "|3:|"++interval++"|4:|average " ++
                     show average ++ " lines|&chxtc=4,-500&chxp=0,100|1,100|3,50|4," ++ show prop ++
                     "&chxs=4,445588,13,-1,lt,990000\"/>"
 
-genHourlyChartUrl out log = do
-    let weeks = analyzeHourly 7 log
+deviations xs = map ((\x -> fromIntegral x ** 2) . flip (-) (mean xs)) xs
+mean xs = round $ fromIntegral (sum xs) / fromIntegral (length xs)
+
+genHourlyChartUrl :: Handle -> Log -> String -> IO ()
+genHourlyChartUrl out log interval = do
+    let weeks = analyzeHourly log interval
         cols = intercalate "," colors
-        fills = map (\(idx, col) -> printf "b,%s,%d,%d,0" col (idx::Int) ((idx+1)::Int)) (zip [0..] colors)
-        dsets = intercalate "|" . map genDataSet . reverse . conv5 . unzip5 . map conv5' . map (scanl (+) 0) $ weeks
-        url = printf "http://chart.apis.google.com/chart?cht=lc&chds=0,100&chs=500x250&chxt=y,x,x&chxp=0,100|2,100&chxl=0:|100%%|2:|Month|&chdl=18-24|12-18|06-12|00-06&chco=%s&chm=%s&chd=t:%s" cols (intercalate "|" fills) dsets
+        fills = map (\(idx, col) -> printf "b,%s,%d,%d,0" col (idx :: Int) (idx + 1 :: Int) (zip [0..] colors)
+        dsets = intercalate "|" . map genDataSet . reverse . conv5 . unzip5 . map (conv5' . scanl (+) 0) $ weeks
+        url = printf "http://chart.apis.google.com/chart?cht=lc&chds=0,100&chs=500x250&chxt=y,x,x&chxp=0,100|2,100&chxl=0:|100%%|2:|%s|&chdl=18-24|12-18|06-12|00-06&chco=%s&chm=%s&chd=t:%s" interval cols (intercalate "|" fills) dsets
     hPutStrLn out "<h2>Monthly activity distribution by hours</h2>"
     hPutStrLn out $ "<img src=\"" ++ url ++ "\"/>"
