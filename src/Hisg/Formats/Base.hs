2165bd1769a01e4acb847c72cc6443161e85831f
Antoine Kalmbach <anhekalm@gmail.com>
2010-09-18 18:00:18 +0300

    Optimization, optimization, optimization, optimization... still using way too much memory

--- src/Hisg/Formats/Base.hs
+++ src/Hisg/Formats/Base.hs
@@ -1,9 +1,14 @@
 module Hisg.Formats.Base (
-    validNickChars
+    validNickChars,
+    validChanChars
     ) where
 
 import qualified Data.ByteString.Char8 as S
 
 -- | Valid nickname characters according to IRC RFC 2812 are A-Z, a-z, {}[]`^\|.
 validNickChars :: String
-validNickChars = "[A-Za-z\\[\\]\\\\`_\\^\\{\\|\\}"
+validNickChars = "A-Za-z\\[\\]\\\\`_\\^\\{\\|\\}"
+
+-- | Valid channel characters are "any octet except NUL, BELL, CR, LF, " ", "," and ":""
+validChanChars :: String
+validChanChars = "^\\r\\n\\0\\s,:"
