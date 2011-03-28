d705f7d9abb7b27b819a11ddfcfe788d59f159f8
ane <anhekalm@gmail.com>
2011-03-28 22:05:09 +0300

    Fix library imports in cabal file and change for DeepSeq.

--- src/Hisg/LineChunks.hs
+++ src/Hisg/LineChunks.hs
@@ -8,7 +8,7 @@ module Hisg.LineChunks
 
 import Control.Exception (bracket, finally)
 import Control.Monad (forM, liftM)
-import Control.Parallel.Strategies (NFData, rnf)
+import Control.DeepSeq (NFData, rnf)
 import Data.Int (Int64)
 import qualified Data.ByteString.Lazy.Char8 as LB
 import GHC.Conc (numCapabilities)
