4f13bcbbbb2aaf0509970b506d98b80ddf1e5639
ane <anhekalm@gmail.com>
2011-03-29 19:04:24 +0300

    Tidied up stats calculation. Everything is done in one chunked operation, but there is a lot of redundancy now. Consider rewriting and removing unused parts.

--- src/Hisg/Stats.hs
+++ src/Hisg/Stats.hs
@@ -16,28 +16,16 @@
 --
 -- For further details, see LICENSE.
 
-module Hisg.Stats (
-  isMessage,
---  getDates,
---  getKicks,
-  calcMessageStats,
-  calcMessageStats'',
-  calcKickStats,
-  processMessages,
-  updateMap
---   getNicks,
-  ) where
+module Hisg.Stats where
 
 import Data.List
-import Data.Maybe
-import Control.Monad
-import Control.Monad.State
+import Control.Parallel.Strategies
 import qualified Data.Map as M
 import qualified Data.Set as Set
 import qualified Data.ByteString.Char8 as S
 import qualified Data.ByteString.Lazy.Char8 as L
-
 import Text.Regex.PCRE.Light (compile, match)
+
 import Hisg.Types
 import Hisg.MapReduce
 import Hisg.Formats.Irssi
@@ -49,83 +37,39 @@ import Hisg.Formats.Irssi
 --instance Eq User where
 --(User n1 w1 l1) == (User n2 w2 l2) = n1 == n2 && w1 == w2 && l1 == l2
 
--- | The StatsM monad allows us to work on a changing set of log events.
--- As a result, we can prune what we want from the log while calculating stats,
--- thus reducing the size of the log file by not having to travel through
--- every bit of (un-lazy) data.
-type StatsM = State Log
+type UserStats = (Int, Int, Int)
+type StatsMap = M.Map S.ByteString UserStats
 
--- | Gets the messages from the current log and updates the state with the remaining non-messages.
-takeMessages :: StatsM Log
-takeMessages = do
-  evts <- get
-  put $ map snd $ (map (partition isMessage) evts)
-  return $ map fst $ (map (partition isMessage) evts)
 
-isMessage (Message _ _ _) = True
-isMessage _ = False
+-- | Calculates statistics for an user, currently lines, words and kicks given.
+-- Will eventually calculate everything you can use integers for, i.e.
+-- joins, quits, parts etc.
+calcUserStats :: [L.ByteString] -> StatsMap
+calcUserStats = mapReduce rseq (foldl' matchAll M.empty . L.lines)
+                   rseq (M.unionsWith sumTriples)
 
-workMessageStats :: StatsM [(S.ByteString, (Int, Int))]
-workMessageStats = do
-  msgs <- takeMessages
-  return $ processMessages msgs
+matchAll :: StatsMap                       -- our line
+         -> L.ByteString  -- our map
+         -> StatsMap  -- our modified map if the line matches
+matchAll m l = matchKick l . matchMessage l $ m
 
---workKickStats :: StatsM [(S.ByteString, Int)]
---workKickStats = diio
---  msgs <- takeKicks
---
-calcMessageStats :: Log -> [(S.ByteString, (Int, Int))]
-calcMessageStats log = processMessages log
+matchMessage :: L.ByteString                      -- our line
+             -> StatsMap  -- our map
+             -> StatsMap  -- our modified map if the line matches
+matchMessage line statsMap = case match (compile normalMessageRegex []) (conv line) [] of
+  Just (_:_:nick:contents)
+    -> M.insertWith' sumTriples nick (1, length . S.words . S.concat $ contents, 0) statsMap
+  _ -> statsMap
 
--- | Calculates statistics for normal messages, returning a map
--- with stats for each nick.
-calcMessageStats'' :: [L.ByteString] -> M.Map S.ByteString (Int, Int)
-calcMessageStats'' = mapReduce rwhnf (foldl' updateWLC M.empty . L.lines)
-                   rwhnf (M.unionsWith (sumTuples))
-  where
-    updateWLC map line =
-      case match (compile normalMessage []) (conv line) [] of
-        Just (_:ts:nick:contents)
-              -> M.insertWith' (sumTuples) nick (1, length . S.words . S.concat $ contents) map
-        _ -> map
+matchMessage :: L.ByteString                      -- our line
+             -> StatsMap  -- our map
+             -> StatsMap  -- our modified map if the line matches
+matchKick line map = case match (compile kickMessageRegex []) (conv line) [] of
+  Just (_:_:_:_:nick:_)
+    -> M.insertWith' sumTriples nick (0, 0, 1) map
+  _ -> map
 
 conv = S.concat . L.toChunks
 
--- | The tuple records kicks and kicks received
-calcKickStats :: [L.ByteString] -> M.Map S.ByteString Int
-calcKickStats = mapReduce rwhnf (foldl' updateKC M.empty . L.lines)
-                          rwhnf (M.unionsWith (+))
-  where
-    updateKC map line =
-        case match (compile kick []) (conv line) [] of
-            Just (_:ts:target:chan:author:reason)
-                -> M.insertWith' (+) author 1 map
-            _ -> map
-
---calcKickStats :: Log -> [(S.ByteString, Int)]
---calcKickStats = evalState (workKickStats)
-
---toUser (n, l, w) = User n l w
---toUser' (n, l) = User n l 0
-
-processMessages :: Log -> [(S.ByteString, (Int, Int))]
-processMessages log = M.toList $ mapReduce rwhnf (foldl' updateWLC M.empty)
-                                             rwhnf (M.unionsWith (sumTuples)) log
-processMessages' :: Log -> [(S.ByteString, Int)]
-processMessages' log = M.toList $ mapReduce rwhnf (foldl' updateWLC' M.empty)
-                                             rwhnf (M.unionsWith (+)) log
-
--- | Alias for insertWith (it's shorter!)
-updateMap :: (Ord k) => (a -> a -> a) -> k -> a -> M.Map k a -> M.Map k a
-updateMap fn key value = M.insertWith fn key value
-
-updateWLC :: M.Map S.ByteString (Int, Int) -> LogEvent -> M.Map S.ByteString (Int, Int)
-updateWLC map (Message ts nick line) = updateMap sumTuples nick (1, length $ S.words line) map
-updateWLC map _ = map
-
-updateWLC' :: M.Map S.ByteString Int -> LogEvent -> M.Map S.ByteString Int
-updateWLC' map (Message ts nick line) = updateMap (+) nick 1 map
-updateWLC' map _ = map
-
-sumTuples (a,b) (c,d) = (a+c, b+d)
+sumTriples (a,b,c) (d,e,f) = (a+d, b+e, c+f)
 
