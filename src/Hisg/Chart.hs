e41eb04a179fecd7cd3edd6a94c91c71a0111dc4
ane <ane@iki.fi>
2011-03-31 20:18:08 +0300

    Produce activity charts, bumps GC time for some reason. Fix that someday. But no more premature optimization!

--- src/Hisg/Chart.hs
+++ src/Hisg/Chart.hs
@@ -25,122 +25,16 @@ import Control.Monad
 import Text.Printf
 import System.IO
 
-import Hisg.Types
-import Hisg.Stats
+siteColours :: [String]
+siteColours = ["330570", "7C1F7C", "996AD6", "8243D6"]
 
-type Interval = String
+generateHourlyActivityBarChart :: [Int] -> String
+generateHourlyActivityBarChart hours = linkImg $ urlBase ++ chartData
+  where
+    linkImg url = "<img src=\"" ++ url ++ "\" />"
+    urlBase = "http://chart.apis.google.com/chart?chbh=a,0,0&chs=140x20&cht=bhs&chco=" ++ colours ++ "&chds=0,1,0,1,0,1,0,1&chd=t:"
+    chartData = intercalate "|" $ map (\x -> show (fromIntegral x / fromIntegral total)) hours
+    total = sum hours
+    colours = intercalate "," siteColours
 
-intervals = [("week", 7),
-             ("month", 30),
-             ("day", 1)]
-
-colors :: [String]
-colors = ["CCFFCC", "66CCFF", "0066FF", "0000CC"]
-
-getDays :: Log -> [[LogEvent]]
-getDays = splitWhen isDate
-    where
-        isDate (DateChange _) = True
-        isDate _ = False
-
-getMonths :: Log -> [[[LogEvent]]]
-getMonths = chunk 30 . getDays
-
-
-getTimestamps :: [LogEvent] -> [Timestamp]
-getTimestamps events = map getTs (filter isMessage events)
-    where
-        getTs (Message ts _ _) = ts
-
-getHours = map ts_hour
-
-avg [] = 0
-avg xs = sum xs / genericLength xs
-
--- Splices a day into four segments by hours (0-6, 6-12, 12-18, 18-24)
-spliceDay :: [LogEvent] -> [[Int]]
-spliceDay log = [a, b, c, d]
-    where
-        a = filter (<6) hours
-        b = filter (\h -> h >= 6 && h < 12) hours
-        c = filter (\h -> h >= 12 && h < 18) hours
-        d = filter (>= 18) hours
-        hours = getHours . getTimestamps $ log
-
--- Proportions the number of lines spoken in a segment to the total number of lines spoken that day.
-proportionPeriodHours :: [[Int]] -> (Double, Double, Double, Double)
-proportionPeriodHours hours = h $ map prop hours
-    where
-        h [a, b, c, d] = (a, b, c, d)
-        tot = sum (map length hours)
-        prop [] = 0.0
-        prop xs = len / fromIntegral tot
-            where
-                len = fromIntegral (length xs)
-
--- Falls back to 1 if it doesn't find a good interval.
--- Should use 7?
-getInterval :: Interval -> Int
-getInterval interval = fromMaybe 1 (lookup interval intervals)
-
-analyzeHourly :: Log -> Interval -> [[Int]]
-analyzeHourly log interval = calcAverages . catenateRows . chunk (getInterval interval) . toDailyHours $ getDays log
-    where
-        -- The moment I wrote this I knew my soul was damned forever.
-        calcAverages = map (map (round . (*100) . avg))
-        catenateRows = map (conv4 . unzip4)
-        toDailyHours = map (proportionPeriodHours . spliceDay)
-
-daysByWeeks xs interval = map length (map head . chunk interval $ xs)
-
-analyzeLineTrend :: [[LogEvent]] -> Interval -> [Int]
-analyzeLineTrend days interval = map propLen linesPerDay
-    where
-        -- Usually divided into 7 days.
-        linesPerDay = daysByWeeks days (getInterval interval)
-        biggest = maximum linesPerDay
-        propLen x = round (100 * (fromIntegral x / fromIntegral biggest))
-
--- Why do I have to write something like this?
-conv5 (a, b, c, d, e) = [a, b, c, d, e]
-conv4 (a, b, c, d) = [a, b, c, d]
-conv5' [a, b, c, d, e] = (a, b, c, d, e)
-conv4' [a, b, c, d] = (a, b, c, d)
-
-genDataSet :: [Int] -> String
-genDataSet [] = ""
-genDataSet xs = intercalate "," (map show xs)
-
-genLineChartUrl :: Handle -> Log -> Interval -> IO ()
-genLineChartUrl out log interval = do
-    let days = getDays log
-        crunched = analyzeLineTrend days interval
-        ylegend = intercalate "|" (map show [1 .. length crunched])
-        dayLines = map length days
-        biggest = maximum dayLines
-        average = round $ fromIntegral (sum dayLines) / fromIntegral (length dayLines) :: Int
-        prop = round (100 * (fromIntegral average / fromIntegral biggest)) :: Int
-
-    hPutStrLn out $ "<h2>Average messages per day</h2>" ++
-                    "<img src=\"" ++
-                    "http://chart.apis.google.com/chart?" ++
-                    "&chm=B,66CCFF,0,1,0" ++
-                    "&cht=lc&chs=500x250&chd=t:" ++
-                    genDataSet crunched ++
-                    "&chco=445588&chxt=y,y,x,x,r&chxl=0:|lines|1:|" ++
-                    show biggest ++ "|2:|" ++ ylegend ++ "|3:|"++interval++"|4:|average " ++
-                    show average ++ " lines|&chxtc=4,-500&chxp=0,100|1,100|3,50|4," ++ show prop ++
-                    "&chxs=4,445588,13,-1,lt,990000\"/>"
-
-deviations xs = map ((\x -> fromIntegral x ** 2) . flip (-) (mean xs)) xs
-mean xs = round $ fromIntegral (sum xs) / fromIntegral (length xs)
-
-genHourlyChartUrl :: Handle -> Log -> String -> IO ()
-genHourlyChartUrl out log interval = do
-    let weeks = analyzeHourly log interval
-        cols = intercalate "," colors
-        fills = map (\(idx, col) -> printf "b,%s,%d,%d,0" col (idx :: Int) (idx + 1 :: Int)) (zip [0..] colors)
-        dsets = intercalate "|" . map genDataSet . reverse . conv5 . unzip5 . map (conv5' . scanl (+) 0) $ weeks
-        url = printf "http://chart.apis.google.com/chart?cht=lc&chds=0,100&chs=500x250&chxt=y,x,x&chxp=0,100|2,100&chxl=0:|100%%|2:|%s|&chdl=18-24|12-18|06-12|00-06&chco=%s&chm=%s&chd=t:%s" interval cols (intercalate "|" fills) dsets
-    hPutStrLn out "<h2>Message distribution by hours</h2>"
-    hPutStrLn out $ "<img src=\"" ++ url ++ "\"/>"
+--0.25,0.35|0.25,0.25|0.15,0.15|0.35,0.25
--- src/Hisg/Stats.hs
+++ src/Hisg/Stats.hs
@@ -29,14 +29,8 @@ import Text.Regex.PCRE.Light (compile, match)
 import Hisg.MapReduce
 import Hisg.Formats.Irssi
 
---instance Ord User where
---  (User n1 w1 l1) <= (User n2 w2 l2) = l1 <= l2
---  (User n1 w1 l1) > (User n2 w2 l2) = l1 > l2
---
---instance Eq User where
---(User n1 w1 l1) == (User n2 w2 l2) = n1 == n2 && w1 == w2 && l1 == l2
-
-type UserStats = (Int, Int, Int)
+-- | lines, words, kicks, night, morning, afternoon, evening
+type UserStats = [Int]
 type StatsMap = M.Map S.ByteString UserStats
 
 
@@ -45,30 +39,44 @@ type StatsMap = M.Map S.ByteString UserStats
 -- joins, quits, parts etc.
 calcUserStats :: [L.ByteString] -> StatsMap
 calcUserStats = mapReduce rseq (foldl' matchAll M.empty . L.lines)
-                   rseq (M.unionsWith sumTriples)
+                   rseq (M.unionsWith sumList)
 
 matchAll :: StatsMap                       -- our line
          -> L.ByteString  -- our map
          -> StatsMap  -- our modified map if the line matches
 matchAll m l = matchKick l . matchMessage l $ m
 
-matchMessage :: L.ByteString                      -- our line
-             -> StatsMap  -- our map
-             -> StatsMap  -- our modified map if the line matches
+matchMessage :: L.ByteString -> StatsMap -> StatsMap  -- our modified map if the line matches
 matchMessage line statsMap = case match (compile normalMessageRegex []) (conv line) [] of
-  Just (_:_:nick:contents)
-    -> M.insertWith' sumTriples nick (1, length . S.words . S.concat $ contents, 0) statsMap
+  Just (_:ts:nick:contents)
+    -> let hours = getHours ts in M.insertWith' sumList nick [1, length . S.words . S.concat $ contents, 0, night hours, morning hours, afternoon hours, evening hours] statsMap
   _ -> statsMap
+  where
+    getHours ts = case (S.readInt ts) of
+                    Just (x, _) -> x
+                    _ -> 0
+    night h | 0 <= h && h < 6 = 1
+            | otherwise = 0
+    morning h | 6 <= h && h < 12 = 1
+              | otherwise = 0
+    afternoon h | 12 <= h && h < 18 = 1
+                | otherwise = 0
+    evening h | 18 <= h && h < 24 = 1
+              | otherwise = 0
 
 matchKick :: L.ByteString                      -- our line
              -> StatsMap  -- our map
              -> StatsMap  -- our modified map if the line matches
 matchKick line map = case match (compile kickMessageRegex []) (conv line) [] of
   Just (_:_:_:_:nick:_)
-    -> M.insertWith' sumTriples nick (0, 0, 1) map
+    -> M.insertWith' sumList nick [0, 0, 1, 0, 0, 0, 0] map
   _ -> map
 
 conv = S.concat . L.toChunks
 
+sumList = zipWith (+)
+
+-- I should REALLY REALLY (YES) implement this stuff as a list. but oh well.
+sumN (a, b, c, d, e, f, g) (a', b', c', d', e', f', g') = (a+a', b+b', c+c', d+d', e+e', f+f', g+g')
 sumTriples (a,b,c) (d,e,f) = (a+d, b+e, c+f)
 
